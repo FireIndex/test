04ae9de28a2c397cbefedc0b29d757b22ad3621f
FireIndex <88580636+FireIndex@users.noreply.github.com>
2023-11-25 10:36:00 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -75,6 +75,7 @@ app.get('/player/:id', (req, res) => {
   res.sendFile(__dirname + '/p.html')
 })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
